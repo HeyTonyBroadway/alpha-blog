e9eb58822f4049897ffba59140ad2128b45f2b17
HeyTonyBroadway <heytonybroadway@gmail.com>
2016-11-09 20:29:27 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+	validates :username, presence: true, uniqueness: { case_sensitive: false}, #ensures uniqueness and looks for case sensitivity
+	length: { minimum: 3, maximum: 25 }
 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #this is a 'regular expression', which can be created using rubular.com
+	validates 	:email, presence: true, length: { maximum: 105 },
+				uniqueness: { case_sensitive: false },
+				format: { with: VALID_EMAIL_REGEX }
 end
